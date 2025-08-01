b4633f696ab9a006c5e3958c4cb8a24f30a413ab
Sedulix_exv <131552382+sedulix@users.noreply.github.com>
2025-08-01 20:44:23 +0500

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -5,21 +5,27 @@ import pygame.image
 
 class Skull:
     def __init__(self):
-        self.image = pygame.image.load("resources/enemy.png")
+        self.image = pygame.image.load("enemies/enemy.png")
         self.width, self.height = 80, 80
         self.x, self.y = randint(0, SCREEN_WIDTH - self.width), 0
         self.step = SKULL_STEP
         self.speed = self.step
 
+
     def update_position(self):
         self.y += self.speed
 
+
     def increase_speed(self):
         self.speed += self.step / 6
+
 
     def reset(self):
         self.increase_speed()
         self.x, self.y = randint(0, SCREEN_WIDTH - self.width), 0
 
+
     def is_reached_ship(self, ship):
         return self.y + self.height > ship.y
+
+
